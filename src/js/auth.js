d46207bf53f69b00ecd1d62c739562db28d62073
Gaddiel <gaddiel@localhost.localdomain>
2021-02-22 01:44:11 +0100

    logout|modified mesurement forms

--- src/js/auth.js
+++ src/js/auth.js
@@ -4,7 +4,20 @@ $(() => {
         "password": "a1outlook"
     }
 
+    const generateKey = (length) => {
+        let counter = 0;
+        let key = "";
+        let chars = 'abcdefghijklmnopqrstuvwxyz1234567890!@#₦%^&*()_+-=';
+        while (counter < length) {
+            key += chars[Math.floor(Math.random() * chars.length - 1)];
+            counter++;
+        }
+        return key;
+    }
+
     $('#login').on('submit', (e) => {
+        // This function handles authentication;
+        
         e.preventDefault();
         const formData = {};
         $('#login').serializeArray().forEach(input => {
@@ -14,7 +27,16 @@ $(() => {
         if(formData.email != authDetails.email && formData.password != authDetails.password){
             window.alert('Email or Password incorrect, Check and try again');
         } else {
+            const key = generateKey(15);
+            window.sessionStorage.setItem('sessionKey', key);
             window.location.href = `src/pages/dashboard.html`;
         }
     })
+
+    $('#signOutBtn').on('click', (e) => {
+        e.preventDefault();
+        window.sessionStorage.removeItem('sessionKey');
+        window.location.href= '/index.html'
+    })
+
 })
