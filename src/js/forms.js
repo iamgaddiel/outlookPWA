66dc42324430a759536acf462a4867b24de27e4b
Gaddiel <gaddiel@localhost.localdomain>
2021-02-24 12:13:49 -0800

    Fixed listing of clients

--- src/js/forms.js
+++ src/js/forms.js
@@ -25,112 +25,128 @@ $(() => {
 
         db.collection(collection).orderBy('id', 'desc').get()
         .then( measurements => {
-            measurements.forEach( data => {
-                
-                if (!!clientList){
+            if ( measurements.length > 0){
 
-                    switch(data.category){
+                measurements.forEach( data => {
+                    
+                    if (!!clientList){
     
-                        case "trouser":
-                            displayData = `
-                                <a href="./client-detail.html" data-id="${data.id}">
-                                    <div class="shadow d-flex justify-content-between align-items-center px-2">
-                                        <div class="recent-item">
-                                            <div class="recent-image">
-                                                <img src="../assets/svg/trousers.svg" alt="">
+                        switch(data.category){
+        
+                            case "trouser":
+                                displayData = `
+                                    <a href="./client-detail.html" data-id="${data.id}">
+                                        <div class="shadow d-flex justify-content-between align-items-center px-2">
+                                            <div class="recent-item">
+                                                <div class="recent-image">
+                                                    <img src="../assets/svg/trousers.svg" alt="">
+                                                </div>
+                                                <div class="recent-text">
+                                                    <h6 class="text-muted">${data.name}</h6>
+                                                    <span class="text-muted">${data.email}</span>
+                                                </div>
                                             </div>
-                                            <div class="recent-text">
-                                                <h6 class="text-muted">${data.name}</h6>
-                                                <span class="text-muted">${data.email}</span>
-                                            </div>
-                                        </div>
-                                        <div class="del-contact">
-                                            <i class="fa fa-times text-danger id="deleteBtn" data-id="${data.id}"></i>
-                                        </div>
-                                    </div>
-                                </a>
-                            `
-                            clientList.innerHTML += displayData;
-                            break;
-    
-                        case "shirt":
-                            displayData = `
-                                <a href="./client-detail.html" data-id="${data.id}">
-                                    <div class="shadow d-flex justify-content-between align-items-center px-2">
-                                        <div class="recent-item">
-                                            <div class="recent-image">
-                                                <img src="../assets/svg/trousers.svg" alt="">
-                                            </div>
-                                            <div class="recent-text">
-                                                <h6 class="text-muted">${data.name}</h6>
-                                                <span class="text-muted">${data.email}</span>
+                                            <div class="del-contact">
+                                                <i class="fa fa-times text-danger id="deleteBtn" data-id="${data.id}"></i>
                                             </div>
                                         </div>
-                                        <div class="del-contact">
-                                            <i class="fa fa-times text-danger id="deleteBtn" data-id="${data.id}"></i>
-                                        </div>
-                                    </div>
-                                </a>
-                            `
-                            clientList.innerHTML += displayData;
-                            break;
-    
-                        case "waistcoat":
-                            displayData = `
-                                <a href="./client-detail.html" data-id="${data.id}">
-                                    <div class="shadow d-flex justify-content-between align-items-center px-2">
-                                        <div class="recent-item">
-                                            <div class="recent-image">
-                                                <img src="../assets/svg/waistcoat.svg" alt="">
+                                    </a>
+                                `
+                                clientList.innerHTML += displayData;
+                                break;
+        
+                            case "shirt":
+                                displayData = `
+                                    <a href="./client-detail.html" data-id="${data.id}">
+                                        <div class="shadow d-flex justify-content-between align-items-center px-2">
+                                            <div class="recent-item">
+                                                <div class="recent-image">
+                                                    <img src="../assets/svg/shirt.svg" alt="">
+                                                </div>
+                                                <div class="recent-text">
+                                                    <h6 class="text-muted">${data.name}</h6>
+                                                    <span class="text-muted">${data.email}</span>
+                                                </div>
                                             </div>
-                                            <div class="recent-text">
-                                                <h6 class="text-muted">${data.name}</h6>
-                                                <span class="text-muted">${data.email}</span>
-                                            </div>
-                                        </div>
-                                        <div class="del-contact">
-                                            <i class="fa fa-times text-danger id="deleteBtn" data-id="${data.id}"></i>
-                                        </div>
-                                    </div>
-                                </a>
-                            `
-                            clientList.innerHTML += displayData;
-                            break;
-    
-                        case "suit":
-                            displayMeasurement = `
-                                <a href="./client-detail.html" data-id="${data.id}">
-                                    <div class="shadow d-flex justify-content-between align-items-center px-2">
-                                        <div class="recent-item">
-                                            <div class="recent-image">
-                                                <img src="../assets/svg/trousers.svg" alt="">
-                                            </div>
-                                            <div class="recent-text">
-                                                <h6 class="text-muted">${data.name}</h6>
-                                                <span class="text-muted">${data.email}</span>
+                                            <div class="del-contact">
+                                                <i class="fa fa-times text-danger id="deleteBtn" data-id="${data.id}"></i>
                                             </div>
                                         </div>
-                                        <div class="del-contact">
-                                            <i class="fa fa-times text-danger id="deleteBtn" data-id="${data.id}"></i>
+                                    </a>
+                                `
+                                clientList.innerHTML += displayData;
+                                break;
+        
+                            case "waistcoat":
+                                displayData = `
+                                    <a href="./client-detail.html" data-id="${data.id}">
+                                        <div class="shadow d-flex justify-content-between align-items-center px-2">
+                                            <div class="recent-item">
+                                                <div class="recent-image">
+                                                    <img src="../assets/svg/waistcoat.svg" alt="">
+                                                </div>
+                                                <div class="recent-text">
+                                                    <h6 class="text-muted">${data.name}</h6>
+                                                    <span class="text-muted">${data.email}</span>
+                                                </div>
+                                            </div>
+                                            <div class="del-contact">
+                                                <i class="fa fa-times text-danger id="deleteBtn" data-id="${data.id}"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                </a>
-                            `
-                            clientList.innerHTML += displayData;
-                            break;
-    
-                        default:
-                            displayData = `
-                            <p class="text-muted text-center">
-                                Opps ! 
-                                No client data could be found
-                            </p>`
-                            clientList.innerHTML += displayData;
+                                    </a>
+                                `
+                                clientList.innerHTML += displayData;
+                                break;
+        
+                            case "suite":
+                                displayData = `
+                                    <a href="./client-detail.html" data-id="${data.id}">
+                                        <div class="shadow d-flex justify-content-between align-items-center px-2">
+                                            <div class="recent-item">
+                                                <div class="recent-image">
+                                                    <img src="../assets/svg/suit.svg" alt="">
+                                                </div>
+                                                <div class="recent-text">
+                                                    <h6 class="text-muted">${data.name}</h6>
+                                                    <span class="text-muted">${data.email}</span>
+                                                </div>
+                                            </div>
+                                            <div class="del-contact">
+                                                <i class="fa fa-times text-danger id="deleteBtn" data-id="${data.id}"></i>
+                                            </div>
+                                        </div>
+                                    </a>
+                                `
+                                clientList.innerHTML += displayData;
+                                break;
+        
+                            default:
+                                displayData = `
+                                <p class="text-muted text-center">
+                                    Opps ! 
+                                    No client data could be found
+                                </p>`
+                                clientList.innerHTML += displayData;
+                        }
                     }
-                }
+    
+                    
+                })
 
-                
-            })
+            } else {
+                displayData = `
+                    <div style="width: 30px; margin: auto;">
+                        <i class="fa fa-frown fa-3x text-muted text-center"></i>
+                    </div>
+                    <p class="text-muted text-center mt-4">
+                    Opps ! 
+                    </p>
+                    <p class="text-muted text-center mt-4">
+                        No client data could be found
+                    </p>`
+                clientList.innerHTML += displayData;
+            }
         })
     }
 
@@ -151,7 +167,7 @@ $(() => {
     // Get form data
     $('#trouserForm').on('submit', (event) => getFormData(event, 'trouserForm'));
     $('#waistcoatForm').on('submit',(event) => getFormData(event, 'waistcoatForm'));
-    $('#shirtForm').on('submit',(event) => getFormData(event, 'shirtForm'));
+    $('#suiteForm').on('submit',(event) => getFormData(event, 'suiteForm'));
     $('#shirtForm').on('submit',(event) => getFormData(event, 'shirtForm'));
 
 })
